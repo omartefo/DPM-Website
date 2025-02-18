f236c5667a297b48dc4462813b657387e85d465d
Farid Ullah <faridullah996@gmail.com>
2025-02-18 12:23:37 +0500

    changed mail to 'info@dohapm.com'

--- src/app/common/constants/constants.ts
+++ src/app/common/constants/constants.ts
@@ -1,6 +1,6 @@
 export enum UserConfig {
     mobileNumberLength = 8,
     contactNumber = 50675888,
-    contactEmail = 'info@dohapm.qa',
+    contactEmail = 'info@dohapm.com',
     contactAddress = 'Office 254, Twin Towers, Tower B, 25th Floor, Lusail Qatar'
 };
